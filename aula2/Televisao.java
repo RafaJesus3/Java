8de37526a5d14348cd87a3e9bc35415f1e9c190e
RafaJesus3 <rafael.sjesus1@outlook.com>
2026-09-10 22:59:36 -0300

    exercicios aula2 adicionados

---
+++ aula2/Televisao.java
@@ -0,0 +1,59 @@
+package aula2;
+
+/**
+ * Encapsulamento
+ * - private (Não acesso interno aos meus atributos) Faço acesso atrás de métodos.
+ * - protected*
+ * - public (
+ */
+
+
+public class Televisao {
+
+    private int canal;
+    private int volume;
+    private boolean ligado;
+    private String marca;
+
+    //Métodos de acesso e modificadores - getters() e setters()
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int volume) {
+        //Regra de negócio / Validação -> Aqui...
+        this.canal = canal;
+
+
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        //Regra de negócio / Validação -> Aqui...
+        this.volume = volume;
+    }
+
+    public boolean getLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        //Regra de negócio / Validação -> Aqui...
+        this.ligado = ligado;
+    }
+
+    public String getMarca(){
+        return marca;
+
+    }
+
+    public void setMarca(String marca) {
+        //Regra de negócio / Validação -> Aqui...
+        this.marca = marca;
+    }
+
+}
---
+++ aula2/TelevisaoTeste.java
@@ -0,0 +1,42 @@
+package aula2;
+
+public class TelevisaoTeste {
+
+    public static void main(String[] args){
+
+        System.out.println(" --- Minha Televisão ---");
+
+        //Criação do objeto de Televisão
+        Televisao tv = new Televisao();
+
+
+
+        System.out.println("Marca: " + tv.getMarca());
+        System.out.println("Canal: " + tv.getCanal());
+        System.out.println("Volume: " + tv.getVolume());
+        System.out.println("Ligado: " + tv.getLigado());
+
+
+        //Alterando os dados do objeto tv (forma direta)
+
+        //tv.getCanal() = 30;
+        tv.setCanal(5000);
+        //tv.getVolume() = 1000;
+        tv.setVolume(-3);
+        //tv.getLigado() = true;
+        tv.setLigado(true);
+        //tv.getMarca() = Sony;
+        tv.setMarca("Sony");
+
+        System.out.println("\n-----------------------------------------\n");
+
+
+        System.out.println("Marca: " + tv.getMarca());
+        System.out.println("Canal: " + tv.getCanal());
+        System.out.println("Volume: " + tv.getVolume());
+        System.out.println("Ligado: " + tv.getLigado());
+
+    }
+
+
+}
